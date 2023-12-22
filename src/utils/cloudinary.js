994ef447639c2ed1efecd9930838f0ad179c68b9
Ravinder Pandey <59820924+ravin972@users.noreply.github.com>
2023-12-22 23:06:13 +0530

    add multer and cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,26 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadOnCloudinary = async (localFilePath) => {
+  try {
+    if (!localFilePath) return null;
+    //upload the file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto",
+    });
+    //files are uploaded successfully
+    console.log("Files uploaded successfully", response.url);
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath); // remove temporary file path from cloudinary
+    return null;
+  }
+};
+
+export { uploadOnCloudinary };
